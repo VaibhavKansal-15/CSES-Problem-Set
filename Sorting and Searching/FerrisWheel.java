5289c8f0fd0991d37527619e2a0192be5d1412c8
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-01-19 21:33:52 +0530

    Update FerrisWheel.java

--- Sorting and Searching/FerrisWheel.java
+++ Sorting and Searching/FerrisWheel.java
@@ -1,12 +1,9 @@
-package SortingandSearching;
-
 import java.io.*;
 import java.util.*;
 public class FerrisWheel {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         long n = Long.parseLong(st.nextToken());
         long x = Long.parseLong(st.nextToken());
         long[] arr = new long[(int) n];
@@ -22,7 +19,6 @@ public class FerrisWheel {
             System.out.println(100000);
             return;
         }
-
         while (i <= j) {
             if (arr[i] + arr[j] <= x) {
                 count++;
