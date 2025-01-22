8a760671dd5c67f9c5b79f52fb41604ce99ff35d
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-01-22 20:36:53 +0530

    Add files via upload

---
+++ Sorting and Searching/MissingCoinSum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+public class MissingCoinSum{
+  public static void main(String[] args) throws IOException{
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer(br.readLine());
+    int n=Integer.parseInt(st.nextToken());
+    long[] arr=new long[n];
+    st=new StringTokenizer(br.readLine());
+    for(int i=0;i<n;i++){
+      arr[i]=Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(arr);
+    long target=1;
+    for(long coin:arr){
+      if(coin>target){
+        break;
+      }
+      target+=coin;
+    } 
+    System.out.println(target);
+  }
+}
