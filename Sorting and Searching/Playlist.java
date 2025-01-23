25f92313b0236530ebfc3be0d4947a59585446ee
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-01-23 21:30:07 +0530

    Add files via upload

---
+++ Sorting and Searching/Playlist.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Playlist {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // Number of songs
+        long[] songs = new long[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            songs[i] = Long.parseLong(st.nextToken());
+        }
+        Set<Long> uniqueSongs = new HashSet<>();
+        int start = 0, maxLength = 0;
+        for (int end = 0; end < n; end++) {
+            while (uniqueSongs.contains(songs[end])) {
+                uniqueSongs.remove(songs[start]);
+                start++;
+            }
+            uniqueSongs.add(songs[end]);
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        System.out.println(maxLength);
+    }
+}
