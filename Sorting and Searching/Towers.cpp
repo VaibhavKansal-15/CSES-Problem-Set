fa02ccc5234e41ec255667ae66e86f47bc16369b
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-01-31 23:24:07 +0530

    Update Towers.cpp

--- Sorting and Searching/Towers.cpp
+++ Sorting and Searching/Towers.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 #include <set>
 using namespace std;
-
 int main() {
     long long n;
     cin >> n;
-
     multiset<long long> towers;
-
     for (long long i = 0; i < n; i++) {
         long long cube;
         cin >> cube;
-
         auto it = towers.upper_bound(cube);
-
         if (it != towers.end()) {
             towers.erase(it);
         }
-
         towers.insert(cube);
     }
-
     cout << towers.size() << endl;
-
     return 0;
 }
