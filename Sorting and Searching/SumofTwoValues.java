36b5e25e7a85254ae4ed0baa75b544a7bc4e5e15
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-01-31 23:24:40 +0530

    Update SumofTwoValues.java

--- Sorting and Searching/SumofTwoValues.java
+++ Sorting and Searching/SumofTwoValues.java
@@ -1,8 +1,5 @@
-//package SortingandSearching;
-
 import java.util.*;
 import java.io.*;
-
 public class SumofTwoValues {
     public static void main(String[] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
