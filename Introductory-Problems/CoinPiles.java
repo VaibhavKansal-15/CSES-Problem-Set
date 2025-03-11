9c82f89cacc92ed01794c9445390a040887da50d
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-03-11 22:12:43 +0530

    Update CoinPiles.java

--- Introductory-Problems/CoinPiles.java
+++ Introductory-Problems/CoinPiles.java
@@ -1,21 +1,17 @@
 // import java.util.Scanner;
-
 // public class CoinPiles {
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
-
 //         long t = sc.nextLong(); // Read the number of test cases
 //         while (t-- > 0) {
 //             long a = sc.nextLong(); // Read the first pile size
 //             long b = sc.nextLong(); // Read the second pile size
-
 //             // Ensure a is always the smaller pile
 //             if (a > b) {
 //                 long temp = a;
 //                 a = b;
 //                 b = temp;
 //             }
-
 //             // Check the conditions for a valid solution
 //             if (b > a * 2 || (a + b) % 3 != 0) {
 //                 System.out.println("NO");
@@ -26,10 +22,6 @@
 //         sc.close();
 //     }
 // }
-
-
-
-
 //Optimised code
 
 import java.io.BufferedReader;
@@ -40,20 +32,17 @@ public class CoinPiles {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder output = new StringBuilder();
-
         int t = Integer.parseInt(br.readLine()); // Number of test cases
         for (int i = 0; i < t; i++) {
             String[] input = br.readLine().split(" ");
             long a = Long.parseLong(input[0]);
             long b = Long.parseLong(input[1]);
-
             // Ensure a is the smaller pile
             if (a > b) {
                 long temp = a;
                 a = b;
                 b = temp;
             }
-
             // Check conditions
             if (b > a * 2 || (a + b) % 3 != 0) {
                 output.append("NO\n");
@@ -61,7 +50,6 @@ public class CoinPiles {
                 output.append("YES\n");
             }
         }
-
         // Print all results at once
         System.out.print(output.toString());
     }
