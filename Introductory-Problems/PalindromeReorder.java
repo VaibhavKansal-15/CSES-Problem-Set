9ec05beaa38c3a28c22099d16c202ff4a193de30
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-03-13 22:26:12 +0530

    Update PalindromeReorder.java

--- Introductory-Problems/PalindromeReorder.java
+++ Introductory-Problems/PalindromeReorder.java
@@ -1,17 +1,16 @@
 import java.util.*;
+
 public class PalindromeReorder {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String s=sc.nextLine();
         sc.close();
-
         //StringBuilder str=new StringBuilder(s.length());
         HashMap<Character,Integer> map=new HashMap<>();
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
             map.put(c,map.getOrDefault(c, 0)+1);
         }
-
         StringBuilder left=new StringBuilder();
         StringBuilder middle=new StringBuilder();
         for(Map.Entry<Character,Integer> entry:map.entrySet()){
@@ -34,7 +33,6 @@ public class PalindromeReorder {
         StringBuilder palindrome=new StringBuilder(left);
         palindrome.append(middle);
         palindrome.append(left.reverse());
-
         System.out.println(palindrome.toString());
     }
 }
